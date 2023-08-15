3f95270aa86d0913ed6e4137c0fe4cf153e27aba
Banele1999 <106697571+Banele1999@users.noreply.github.com>
2023-08-15 08:04:31 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -2,10 +2,10 @@
 
 // -------------About Tabs-------------------
 const tabsContainer = document.querySelector(".about-tabs"),
-aboutSection = document.querySelector(".about-section");
+    aboutSection = document.querySelector(".about-section");
 
 tabsContainer.addEventListener("click", (e) => {
-    if(e.target.classList.contains("tab-item") && !e.target.classList.contains("active")){
+    if (e.target.classList.contains("tab-item") && !e.target.classList.contains("active")) {
         tabsContainer.querySelector(".active").classList.remove("active");
         e.target.classList.add("active");
         const target = e.target.getAttribute("data-target");
